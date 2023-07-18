0d23f8136166f04fad95e90caba5409962544980
Alexey Kabanin <alexey.kabanin@veeam.com>
2023-07-18 19:51:17 +0400

    fixing

--- source/js/modules/tabs/tabs.js
+++ source/js/modules/tabs/tabs.js
@@ -47,4 +47,23 @@ const onControlTabClick = (evt) => {
 
 tabsControl.addEventListener('click', onControlTabClick);
 
+function smoothScrollToAnchor(anchorId) {
+  const anchor = document.querySelector(anchorId);
+
+  if (anchor) {
+    window.scrollTo({
+      top: anchor.offsetTop,
+      behavior: 'smooth',
+    });
+  }
+}
+
+const anchorLink = document.querySelector('.hero__btn');
+
+anchorLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  const targetId = anchorLink.getAttribute('href');
+  smoothScrollToAnchor(targetId);
+});
+
 export {tabsContentItems, tabsContentBtns};
