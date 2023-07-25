322760d33e25dd15fc5e9c9d7573aec8c6136809
Alexey Kabanin <alexey.kabanin@veeam.com>
2023-07-25 12:40:13 +0400

    tabs fixes

--- source/js/modules/tabs/tabs.js
+++ source/js/modules/tabs/tabs.js
@@ -66,4 +66,13 @@ anchorLink.addEventListener('click', (event) => {
   smoothScrollToAnchor(targetId);
 });
 
+tabsContentItems.forEach((item) => {
+  const shadowPriceElement = item.querySelector('[data-shadow-price]');
+  const priceElement = item.querySelector('[data-price]');
+
+  if (priceElement && shadowPriceElement) {
+    shadowPriceElement.textContent = priceElement.textContent;
+  }
+});
+
 export {tabsContentItems, tabsContentBtns};
